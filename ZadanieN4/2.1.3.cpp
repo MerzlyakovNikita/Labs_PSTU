b0bacd5830133efcd8ace0e3526225767382e2d2
Nikita Merzlyakov <92156734+MerzlyakovNikita@users.noreply.github.com>
2021-11-03 19:23:05 +0500

    Create 2.1.3.cpp

---
+++ ZadanieN4/2.1.3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int a = 10;
+	int b, c;
+	b = 20;
+	c = 20;
+	c--;
+	cout << a << endl << b << endl << c << endl;
+	return 0;
+}
