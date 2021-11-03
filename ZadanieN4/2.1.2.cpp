f840b29192050f0b5b1847db2163b1b73d704393
Nikita Merzlyakov <92156734+MerzlyakovNikita@users.noreply.github.com>
2021-11-03 19:06:25 +0500

    Update 2.1.2.cpp

--- ZadanieN4/2.1.2.cpp
+++ ZadanieN4/2.1.2.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "ru");
 	float gal, fut;
+	cout << "Введите число галлонов: ";
 	cin >> gal;
 	fut = gal / 7.481;
-	cout << fut << endl;
+	cout << "Объём в кубических футах равен: " << fut << endl;
 	return 0;
 }
